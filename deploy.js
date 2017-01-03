089032c794ed7de0d9feeac29b4b7d940e5c4e65
Oswald De Riemaecker <oswald@continuousphp.com>
2017-01-03 18:33:45 +0100

    Adding output to package_path

--- deploy.js
+++ deploy.js
@@ -1,12 +1,12 @@
 var deploy = require('heroku-deploy-tarball');
 
-var package_path = process.env.PACKAGE_PATH;
+package_path = process.env.PACKAGE_PATH;
 
 console.log(package_path);
 
 var config = {
   app: 'heroku-demo-zf-apigility-phinx',
-  tarball: package_path
+  tarball: this.package_path
 }
  
 deploy(config);
